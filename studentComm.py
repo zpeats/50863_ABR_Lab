4b2a7d0c2906674bad14163e472bcf0161b7a9d9
zpeats <zpeats@purdue.edu>
2020-04-18 20:51:37 -0400

    Add Student Communication code

---
+++ studentComm.py
@@ -0,0 +1,45 @@
+import socket
+import json
+import studentcodeEX
+
+# Quick-and-dirty TCP Server:
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.bind(('localhost', 6000))
+ss.listen(10)
+
+
+(clientsocket, address) = ss.accept()
+
+
+def recv_commands():
+    message = ""
+
+
+    while(1):
+        messagepart = clientsocket.recv(2048).decode()
+
+        #print(messagepart)
+        message += messagepart
+        if message[-1] == '\n':
+            #print(message)
+
+            jsonargs = json.loads(messagepart)
+            print(jsonargs)
+            message = ""
+
+            #todo: json data sanitization
+            bitrate = studentcodeEX.student_entrypoint(jsonargs["Measured Bandwidth"], jsonargs["Previous Throughput"], jsonargs["Buffer Occupancy"], jsonargs["Available Bitrates"], jsonargs["Video Time"], jsonargs["Chunk"], jsonargs["Rebuffering Time"], jsonargs["Preferred Bitrate"])
+
+            payload = json.dumps({"bitrate" : bitrate}) + '\n'
+            print(payload)
+            clientsocket.sendall(payload.encode())
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    recv_commands()
