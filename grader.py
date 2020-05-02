efb3cbde707bc3adad8126ad313fdf6bbaf011c3
zpeats <zpeats@purdue.edu>
2020-05-02 00:57:58 -0400

    Added scoring functionality to the grader

--- grader.py
+++ grader.py
@@ -18,6 +18,9 @@ if __name__ == "__main__":
     if "-v" in sys.argv or "--verbose" in sys.argv:
         verboseflag = "-v"
 
+    switch_ratio = 1
+    buffer_ratio = 1
+
 
 
     manifestfilename = 'manifest.json'
@@ -43,11 +46,48 @@ if __name__ == "__main__":
 
         student_thread.join()
 
+        #decode output and come up with a final score
+
+        outputlines = output.stdout.decode('unicode_escape').split('\n')
+
+        sanitizedoutput = [line.strip() for line in outputlines]
+
         outtext.append(testdir + ": ")
         outtext.append("\n")
-        outtext.append(output.stdout.decode('unicode_escape'))
+        outtext += outputlines
         outtext.append("\n")
-        outtext.append("\n")
+
+        average_bitrate = None
+        buffer_time = None
+        switches = None
+
+        for line in sanitizedoutput:
+            if "Average bitrate" in line:
+                average_bitrate = float(line.split(':')[1])
+
+            if "buffer time" in line:
+                buffer_time = float(line.split(':')[1])
+
+            if "switches" in line:
+                switches = float(line.split(':')[1])
+
+        if switches is not None and buffer_time is not None and average_bitrate is not None:
+
+            buffer_penalty = pow( (1 - (.05 * buffer_ratio)), buffer_time)
+            switch_penalty = pow( (1 - (.08 * switch_ratio)), switches)
+
+            score = average_bitrate * buffer_penalty * switch_penalty
+
+            outtext.append("Score:")
+            outtext.append(str(score))
+            outtext.append('\n')
+            outtext.append('\n')
+            outtext.append('\n')
+
+        else:
+            outtext.append("Unexpected output from the simulator")
+
+
 
     with open("grade.txt", 'w', encoding='utf-8') as outfile:
         outfile.writelines(outtext)
